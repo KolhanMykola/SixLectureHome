d2bf18437e19ba1ebf5643e8aa724002f2855dec
KolhanMykola <43582002+KolhanMykola@users.noreply.github.com>
2018-10-21 13:33:17 +0300

    .
    .

---
+++ NumberClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumberLibrary;
+
+namespace NumberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+            Console.WriteLine("Input number to check if it is prime:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (number.IsPrime(input))
+            {
+                Console.WriteLine("It is prime");
+            }
+            else
+            {
+                Console.WriteLine("It is not prime");
+            }
+
+            Console.WriteLine("Input number to calculate Fibonacci:");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number.Fibonacci(input));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberLibrary/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberLibrary
+{
+    public class Numbers
+    {
+        public bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int sqrt = (int)Math.Sqrt(number);
+
+            for (int i = 3; i <= sqrt; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public int Fibonacci(int number)
+        {
+            if (number <= 1) return 1;
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+}
